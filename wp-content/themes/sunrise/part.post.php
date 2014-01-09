43adaf0d6bcaa9690c1f8b619d54fbd8599fbdd2
Mike Su <sudo@sudocoda.com>
2014-01-08 22:04:28 -0500

    alignment issues, social icons

--- wp-content/themes/sunrise/part.post.php
+++ wp-content/themes/sunrise/part.post.php
@@ -10,7 +10,7 @@
 	<?php else: ?><?php endif; ?>
 
 
-	<h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+	<h2 class="post-title"><?php the_title(); ?></h2>
 
 	<div class="post-content">
 		<?php the_content(__('Read More', 'site5framework')); ?>
