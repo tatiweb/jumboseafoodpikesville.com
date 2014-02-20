bc022104b1aad9758faf6164b38dd22c5c1cdc51
Mike Su <sudo@sudocoda.com>
2014-02-20 00:43:59 -0500

    gradient to title on homepage

--- wp-content/themes/dimsemenov-Touchfolio-7c2e0cc/footer.php
+++ wp-content/themes/dimsemenov-Touchfolio-7c2e0cc/footer.php
@@ -9,29 +9,18 @@
  */
 ?>
 <div class="ftr">
-<div class="contact">  48 E. Sudbrook Lane<br> Pikesville, MD &nbsp; 21208<br>Phone: 410-602-1441, 401-602-1442</div>
-  <div class="hours">M: Closed<br>Tu-Th: 11:30AM-10:00PM<br>Fr&Sa: 11:30AM-10:30PM<br>Su: 12:00PM-10:00PM
-</div><br/>
-	
-		<ul class="social">
-			<li><a href="http://www.facebook.com/pages/Jumbo-Seafood/111466698894138" class="facebook" title="<?php _e( 'Facebook', 'site5framework' ); ?>"><?php _e( 'Facebook', 'site5framework' ); ?></a></li><?php } ?>
-			<li><a href="http://plus.google.com/115671432646571840384" title="<?php _e( 'Twitter', 'site5framework' ); ?>"><?php _e( 'Twitter', 'site5framework' ); ?></a></li><?php } ?>
-			<li><a href="http://www.yelp.com/biz/jumbo-seafood-chinese-restaurant-pikesville" class="youtube" title="<?php _e( 'Youtube', 'site5framework' ); ?>"><?php _e( 'Youtube', 'site5framework' ); ?></a></li><?php } ?>
-			<li><a href="http://www.urbanspoon.com/r/31/351510/restaurant/Baltimore/Jumbo-Seafood-Chinese-Pikesville" class="gplus" title="<?php _e( 'Google+', 'site5framework' ); ?>"><?php _e( 'Google+', 'site5framework' ); ?></a></li><?php } ?>
-		</ul>
-				<?php } ?>
-<!-- <div class="social">
-	<a href="http://www.facebook.com/pages/Jumbo-Seafood/111466698894138"><img src="/jumboseafoodpikesville.com/wp-content/images/facebook.jpg"/></a>
-	<a href="https://plus.google.com/115671432646571840384/"><img src="/jumboseafoodpikesville.com/wp-content/images/google.jpg"/></a>
-	<a href="http://www.yelp.com/biz/jumbo-seafood-chinese-restaurant-pikesville"><img src="/jumboseafoodpikesville.com/wp-content/images/yelp.jpg"/></a>
-	<a href="http://www.urbanspoon.com/r/31/351510/restaurant/Baltimore/Jumbo-Seafood-Chinese-Pikesville"><img src="/jumboseafoodpikesville.com/wp-content/images/urbanspoon.jpg"/></a> -->
-	<!--
-<a href="#" class="facebook"></a>
-	<a href="#" class="google"></a>
-	<a href="#" class="yelp"></a>
--->
-<!-- </div> -->
-  </div></div>
+	<div class="oneThird"> 48 E. Sudbrook Lane<br> Pikesville, MD &nbsp; 21208<br>Phone: 410-602-1441, 401-602-1442</div>
+	  <div class="oneThird">M: Closed<br>Tu-Th: 11:30AM-10:00PM<br>Fr&Sa: 11:30AM-10:30PM<br>Su: 12:00PM-10:00PM
+	</div>
+
+	<br/>
+	<div class="oneThird">
+		<a href="http://www.facebook.com/pages/Jumbo-Seafood/111466698894138" class="facebook">Facebook</a> |
+		<a href="http://plus.google.com/115671432646571840384/" class="google">Google+</a> |
+		<a href="http://www.yelp.com/biz/jumbo-seafood-chinese-restaurant-pikesville" class="yelp">Yelp</a> |
+		<a href="http://www.urbanspoon.com/r/31/351510/restaurant/Baltimore/Jumbo-Seafood-Chinese-Pikesville" class="urbanspoon">Urban Spoon</a>
+	</div>
+</div>
 <?php // this semantics is such a boring thing ?>
 </div>
 </div>
