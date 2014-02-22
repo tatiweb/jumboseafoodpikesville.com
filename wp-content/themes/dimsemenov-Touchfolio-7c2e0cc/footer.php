d6daebc6589c554afcebae8480371a34e67069fc
Mike Su <sudo@sudocoda.com>
2014-02-22 14:04:16 -0500

    refinements to nav, color scheme, positioning of the navigation elements

--- wp-content/themes/dimsemenov-Touchfolio-7c2e0cc/footer.php
+++ wp-content/themes/dimsemenov-Touchfolio-7c2e0cc/footer.php
@@ -10,13 +10,10 @@
 ?>
 <div class="ftr">
 	<div class="oneThird"> 48 E. Sudbrook Lane<br> Pikesville, MD &nbsp; 21208<br>Phone: 410-602-1441, 401-602-1442</div>
-	  <div class="oneThird">M: Closed<br>Tu-Th: 11:30AM-10:00PM<br>Fr&Sa: 11:30AM-10:30PM<br>Su: 12:00PM-10:00PM
-	</div>
-
-	<br/>
+	<div class="oneThird">M: Closed<br>Tu-Th: 11:30AM-10:00PM<br>Fr&Sa: 11:30AM-10:30PM<br>Su: 12:00PM-10:00PM</div>
 	<div class="oneThird">
 		<a href="http://www.facebook.com/pages/Jumbo-Seafood/111466698894138" class="facebook">Facebook</a> |
-		<a href="http://plus.google.com/115671432646571840384/" class="google">Google+</a> |
+		<a href="http://plus.google.com/115671432646571840384/" class="google">Google+</a> <br/>
 		<a href="http://www.yelp.com/biz/jumbo-seafood-chinese-restaurant-pikesville" class="yelp">Yelp</a> |
 		<a href="http://www.urbanspoon.com/r/31/351510/restaurant/Baltimore/Jumbo-Seafood-Chinese-Pikesville" class="urbanspoon">Urban Spoon</a>
 	</div>
