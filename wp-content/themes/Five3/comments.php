ab6783ecea0b13f5ab8b8ebbea7c1db49af70af4
Mike Su <sudo@sudocoda.com>
2014-02-22 14:18:43 -0500

    menu section header style, everything futura

--- wp-content/themes/Five3/comments.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * The default template for content
- *
- * @package five3.me
- * @since 1.0
- */
-
-?>
--- wp-content/themes/esplanade/sidebar-bottom.php
+++
@@ -1,6 +0,0 @@
-<?php if( is_active_sidebar( 5 ) ) : ?>
-	<div id="sidebar-bottom" class="widget-area" role="complementary">
-		<?php dynamic_sidebar( 5 ) ; ?>
-		<div class="clear"></div>
-	</div><!-- #sidebar-bottom -->
-<?php endif; ?>
